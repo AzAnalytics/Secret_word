e598127b2a6d619f843a523b8b8ca097bd073305
Alex3132 <alexis.zueras.pro@gmail.com>
2024-03-05 15:19:01 +0100

    init commit

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ import os
 
 try:
 
-    user = str(os.environ.get("MONGO_USER"))
+    user = os.environ.get("MONGO_USER")
     password = os.environ.get("MONGO_PASSWORD")
     host = os.environ.get("MONGO_HOST")
     dbname = os.environ.get("MONGO_DBNAME")
     collection_name = os.environ.get("MONGO_COLLECTION_NAME")
     app_name = os.environ.get("MONGO_APP_NAME")
-    uri = f"mongodb+srv://{user}:{password}@{host}/{dbname}?retryWrites=true&w=majority&appName={app_name}&tls=true"
+    uri = f"mongodb://{user}:{password}@{host}/{dbname}?retryWrites=true&w=majority&appName={app_name}&tls=true"
 
     print(uri)
     client = MongoClient(uri)
